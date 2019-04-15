0b6f6c1ac4d4bcfaea5383bd8af12820f3bd439c
FernandoPogo <40005978+FernandoPogo@users.noreply.github.com>
2019-04-14 19:57:35 -0500

    Metodos y Status

--- 01-HTTP/02-Servidor-Web-Con-Nodejs/api-web/src/app.controller.ts
+++ 01-HTTP/02-Servidor-Web-Con-Nodejs/api-web/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,31 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post()
+  postHello() {
+    return 'Hola mi nuevo mundo en post';
+  }
 }
+/*
+@nombreDecorador() //Se usan antes de instanciar atributos, metodos o parametros en decoradores
+class usuario{
+  @atributo)=
+  atributo_publico;
+  private atributo_privado;
+  protected atributo_protegido;
+
+  constructor(@ Parametro() atributo_publico, atributo_privado, atributo_protegido){
+    this.atributo_publico = atributo_publico;
+    this.atributo_privado = atributo_privado;
+    this.atributo_protegido = atributo_protegido;
+  }
+
+  @MetodoA()
+  public metodoPublico(@ParametroA() a){}
+  @MetodoB()
+  private metodoPrivado(){}
+  protected metodoProtegido(){}
+}
+*/
+
